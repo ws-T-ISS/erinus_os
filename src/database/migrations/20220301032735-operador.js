c52005d50c2fb1d187d1cac5315dcfbaaabf9a0e
Ilan Santos <ilansalvador@hotmail.com>
2022-03-01 08:54:52 -0300

    Migration da tabela operador criado

--- src/database/migrations/20220301032735-operador.js
+++ src/database/migrations/20220301032735-operador.js
@@ -1,21 +1,48 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up (queryInterface, DataTypes) {
+    queryInterface.createTable('tb_operador', {
+        codigo: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        usuario: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        senha: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        ativo: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false
+        },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down (queryInterface) {
+    return queryInterface.dropTable('tb_operador')
   }
 };
