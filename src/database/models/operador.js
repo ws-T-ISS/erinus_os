e3f285188760257f83780a6bebc7fabc3ecd907c
Ilan Santos <ilansalvador@hotmail.com>
2022-03-05 14:53:34 -0300

    Criado modelo padrão de operador

--- src/database/models/operador.js
+++ src/database/models/operador.js
@@ -1,36 +1,39 @@
-module.exports = (sequelize, DataTypes) => {
-    const Operador = sequelize.define('Operador', {
-        codigo: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false
-        },
-        nome: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        usuario: {
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false
-        },
-        senha: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        ativo: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        }
-    },{
-        timestamps: true,
-        tableName: 'tb_operador'
-    });
-    return Operador;
-}
+import { Sequelize, DataTypes } from "sequelize"
+import config from '../config/database'
+const sequelize = new Sequelize(config)
+
+const Operador = sequelize.define('Operador', {
+    codigo: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    usuario: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ativo: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+},{
+    timestamps: true,
+    tableName: 'tb_operador'
+});
+
+export default Operador
